e827160bd013fa04bf42a8486aec5dc32420dafc
손민기 <smg8932@naver.com>
2022-03-11 16:04:18 +0900

    feat: frontend skeleton

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import 'semantic-ui-css/semantic.min.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ frontend/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <p>메인페이지</p>
+    </>
+  )
+}
+
+export default Home
