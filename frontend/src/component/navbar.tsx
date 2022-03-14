1efafc87cee668b0d54612b3d6ab57ca9c33075a
jodie9596@gmail.com <jodie9596@gmail.com>
2022-03-14 10:35:02 +0900

    feat: navbar base

--- frontend/src/component/navbar.tsx
+++ frontend/src/component/navbar.tsx
@@ -1,7 +1,33 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 export default function Navbar(){
-    return(
-        <>
-            <p>여기에 네브바 만드세요...</p>
-        </>
-    );
+    const router = useRouter();
+    return (
+        <nav>
+          <Link href="/">
+            <a className={router.pathname === "/" ? "active" : ""}>Home</a>
+          </Link>
+          <Link href="/info">
+            <a className={router.pathname === "/info" ? "active" : ""}>info</a>
+          </Link>
+          <Link href="/rec">
+            <a className={router.pathname === "/rec" ? "active" : ""}>rec</a>
+          </Link>
+          <Link href="/notice">
+            <a className={router.pathname === "/notice" ? "active" : ""}>notice</a>
+          </Link>
+          <Link href="/board">
+            <a className={router.pathname === "/board" ? "active" : ""}>board</a>
+          </Link>
+          <style jsx>{`
+            a {
+              text-decoration: none;
+            }
+            .active {
+              color: red;
+            }
+          `}</style>
+        </nav>
+      );
 }
