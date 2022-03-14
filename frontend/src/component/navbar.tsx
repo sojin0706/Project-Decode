c3025e88be6e69d3c11738906acf2f2213685ea2
jodie9596@gmail.com <jodie9596@gmail.com>
2022-03-14 10:38:59 +0900

    feat: navbar space

--- frontend/src/component/navbar.tsx
+++ frontend/src/component/navbar.tsx
@@ -6,19 +6,19 @@ export default function Navbar(){
     return (
         <nav>
           <Link href="/">
-            <a className={router.pathname === "/" ? "active" : ""}>Home</a>
+            <a className={router.pathname === "/" ? "active" : ""}>Home  </a>
           </Link>
           <Link href="/info">
-            <a className={router.pathname === "/info" ? "active" : ""}>info</a>
+            <a className={router.pathname === "/info" ? "active" : ""}>info  </a>
           </Link>
           <Link href="/rec">
-            <a className={router.pathname === "/rec" ? "active" : ""}>rec</a>
+            <a className={router.pathname === "/rec" ? "active" : ""}>rec  </a>
           </Link>
           <Link href="/notice">
-            <a className={router.pathname === "/notice" ? "active" : ""}>notice</a>
+            <a className={router.pathname === "/notice" ? "active" : ""}>notice  </a>
           </Link>
           <Link href="/board">
-            <a className={router.pathname === "/board" ? "active" : ""}>board</a>
+            <a className={router.pathname === "/board" ? "active" : ""}>board  </a>
           </Link>
           <style jsx>{`
             a {
