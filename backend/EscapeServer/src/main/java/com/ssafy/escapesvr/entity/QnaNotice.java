39b1fbb8f49e44bade10f558b7c5d8ebc2a56bfe
오윤진 <funnyjin710@gmail.com>
2022-03-14 11:07:56 +0900

    초기설정

---
+++ backend/EscapeServer/src/main/java/com/ssafy/escapesvr/entity/QnaNotice.java
@@ -0,0 +1,48 @@
+package com.ssafy.escapesvr.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class QnaNotice {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "qna_id")
+    private Long id; //공지사항글 ID
+
+    @NotNull
+    private String title; //제목
+
+    @NotNull
+    private String content; //내용
+
+    @NotNull
+    private boolean isSecret; //보안글여부
+
+    @NotNull
+    private boolean isNotice; //공지글여부
+
+    @NotNull
+    private LocalDateTime createdAt; //작성시간
+
+    @NotNull
+    private LocalDateTime modifiedAt; //수정시간
+
+    @NotNull
+    private Integer user_id;
+
+
+}
