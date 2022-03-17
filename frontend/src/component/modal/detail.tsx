934fb45f149b5df849958d4de7e676ab73655151
손민기 <smg8932@naver.com>
2022-03-17 14:09:09 +0900

    feat: image와 detail modal연결 작업(infopage)

--- frontend/src/component/modal/detail.tsx
+++ frontend/src/component/modal/detail.tsx
@@ -2,17 +2,23 @@ import { useState } from 'react';
 import { Button, Header, Image, Modal, Rating } from 'semantic-ui-react'
 import Reviewboard from '../board/reviewboard';
 
-export default function Detail({item}: any){
+export default function Detail({item, isImage, w, h}: any){
 
     const [open, setOpen] = useState(false)
-
+    console.log('111', isImage)
     return (
         <>
             <Modal
                 onClose={() => setOpen(false)}
                 onOpen={() => setOpen(true)}
                 open={open}
-                trigger={<Button>{item.name}</Button>}
+                trigger={isImage?
+                    <button><img src={item.url} alt="url" width={w} height={h} /></button>
+                :
+                    <Button>{item.name}</Button>
+                }
+                // trigger={<Button><img src={item.name} alt="url" width="150" height="150" /></Button>}
+                // trigger={<button><img src={item.url} alt="url" width="150" height="150" /></button>}
                 >
                 <Modal.Content image>
                     <img src="https://next-edition.s3.amazonaws.com/theme/title_image_url/%EC%99%84%EC%A0%84%ED%95%9C%EC%82%AC%EB%9E%91(%EB%A6%AC%EB%89%B4%EC%96%BC)/theme__%E1%84%8B%E1%85%AA%E1%86%AB%E1%84%8C%E1%85%A5%E1%86%AB%E1%84%92%E1%85%A1%E1%86%AB%E1%84%89%E1%85%A1%E1%84%85%E1%85%A1%E1%86%BC-%E1%84%91%E1%85%A9%E1%84%89%E1%85%B3%E1%84%90%E1%85%A5_%EC%99%84%EC%A0%84%ED%95%9C%EC%82%AC%EB%9E%91(%EB%A6%AC%EB%89%B4%EC%96%BC).jpg" alt="이미지가 없습니다" width="250px" height="350px"/>
