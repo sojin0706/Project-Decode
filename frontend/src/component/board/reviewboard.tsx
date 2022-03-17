fdf0dd396c48c35d91937eeb2f7d0b57b45a2356
손민기 <smg8932@naver.com>
2022-03-17 13:19:22 +0900

    feat: detailpage 댓글 작성, 삭제 특 작성

--- frontend/src/component/board/reviewboard.tsx
+++ frontend/src/component/board/reviewboard.tsx
@@ -1,4 +1,5 @@
-import { Grid, Header, Rating } from "semantic-ui-react";
+import { useState } from "react";
+import { Button, Grid, Header, Icon, Rating } from "semantic-ui-react";
 
 export default function Reviewboard(){
 
@@ -8,6 +9,7 @@ export default function Reviewboard(){
             nickname: '닉네임1',
             score: 3,
             create_date: '2020.09.28',
+            clear_time: 55,
             content: '재밌었다.'
         },
         {
@@ -15,6 +17,7 @@ export default function Reviewboard(){
             nickname: '닉네임2',
             score: 5,
             create_date: '2020.07.28',
+            clear_time: 45,
             content: '추천'
         },
         {
@@ -22,13 +25,15 @@ export default function Reviewboard(){
             nickname: '닉네임3',
             score: 1,
             create_date: '2020.05.28',
-            content: '어려워요'
+            clear_time: 60,
+            content: '어려워요어려워요어려워요어려워요어려워요어려워요어려워요어려워요어려워요어려워요어려워요어려워요어려워요어려워요어려워요어려워요어려워요어려워요어려워요어려워요어려워요어려워요'
         },
         {
             number: 4,
             nickname: '닉네임4',
             score: 4,
             create_date: '2020.03.28',
+            clear_time: 50,
             content: '재밌었다.'
         },
         {
@@ -36,17 +41,55 @@ export default function Reviewboard(){
             nickname: '닉네임5',
             score: 3,
             create_date: '2020.01.28',
+            clear_time: 55,
             content: '난이도 보통'
         },
     ]
-    console.log(reviews)
+
+    const [myRate, setMyRate] = useState(3)
+    const [myTime, setMytime] = useState(0)
+    const [myReview, setMyReview] = useState('')
+
+    function changeTime(e: any){
+        setMytime(e.target.value)
+    }
+
+    function changeRate(e: any){
+        setMyRate(e.target.ariaPosInSet)
+    }
+
+    function changeReview(e: any){
+        setMyReview(e.target.value)
+    }
+
+    function submitReview(e: any){
+        if (!myTime && myTime < 30 || myTime > 90){
+            alert('클리어 시간은 30 ~ 90 사이 숫자로만 작성이 가능합니다.')
+            return
+        }
+        if (myReview.length > 200 || myReview.length < 1){
+            alert('리뷰는 1자 이상 200자 이하로 작성해주세요')
+            return
+        }
+        setMytime(0)
+        setMyReview('')
+        console.log('전달할 예정인 정보', myRate, myTime, myReview)
+        let timeinput: any = document.getElementsByClassName('timeinput')[0]
+        timeinput['value'] = 0
+        let reviewinput: any = document.getElementsByClassName('reviewinput')[0]
+        reviewinput['value'] = ''
+        alert('리뷰가 저장되었습니다.')
+    }
+
+    function deleteReview(){
+        console.log('리뷰 삭제')
+    }
 
     return(
         <>
             <Grid centered stackable>
                 <Grid.Row>
-                    <Grid.Column width={1} />
-                    <Grid.Column width={2}>
+                    <Grid.Column width={1}>
                         <Header as='h5'>번호</Header>
                     </Grid.Column>
                     <Grid.Column width={2}>
@@ -55,19 +98,20 @@ export default function Reviewboard(){
                     <Grid.Column width={2}>
                         <Header as='h5'>별점</Header>
                     </Grid.Column>
-                    <Grid.Column width={3}>
+                    <Grid.Column width={2}>
                         <Header as='h5'>작성날짜</Header>
                     </Grid.Column>
-                    <Grid.Column width={5}>
+                    <Grid.Column width={2}>
+                        <Header as='h5'>클리어시간</Header>
+                    </Grid.Column>
+                    <Grid.Column width={7}>
                         <Header as='h5'>후기</Header>
                     </Grid.Column>
-                    <Grid.Column width={1} />
                 </Grid.Row>
                 {reviews.map((review) => {
                     return(
                         <Grid.Row>
-                            <Grid.Column width={1} />
-                            <Grid.Column width={2}>
+                            <Grid.Column width={1}>
                                 <Header as='h5'>{review.number}</Header>
                             </Grid.Column>
                             <Grid.Column width={2}>
@@ -76,16 +120,52 @@ export default function Reviewboard(){
                             <Grid.Column width={2}>
                                 <Header as='h5'><Rating icon='star' defaultRating={review.score} maxRating={review.score} /></Header>
                             </Grid.Column>
-                            <Grid.Column width={3}>
+                            <Grid.Column width={2}>
                                 <Header as='h5'>{review.create_date}</Header>
                             </Grid.Column>
-                            <Grid.Column width={5}>
+                            <Grid.Column width={2}>
+                                <Header as='h5'>{review.clear_time}분</Header>
+                            </Grid.Column>
+                            <Grid.Column width={6}>
                                 <Header as='h5'>{review.content}</Header>
                             </Grid.Column>
-                            <Grid.Column width={1} />
+                            <Grid.Column width={1}>
+                                <Icon name="trash" color="red" style={{ cursor: 'pointer'}} onClick={deleteReview}/>
+                                {/* <Button size="mini"
+                                    content='X'
+                                    color='red'
+                                /> */}
+                            </Grid.Column>
                         </Grid.Row>
                     );
                 })}
+                <Grid.Row />
+                <Grid.Row>
+                    <Grid.Column width={2}>
+                        <Header as='h5'>닉네임</Header>
+                        <p>내 닉네임</p>
+                    </Grid.Column>
+                    <Grid.Column width={2}>
+                        <Header as='h5'>별점</Header>
+                        <Rating className='scoreinput' icon='star' defaultRating={myRate} maxRating={5} onClick={changeRate}/>
+                    </Grid.Column>
+                    <Grid.Column width={2}>
+                        <Header as='h5'>클리어 시간</Header>
+                        <input className='timeinput' type="number" min={30} max={90} onChange={changeTime} />분
+                    </Grid.Column>
+                    <Grid.Column width={7}>
+                        <Header as='h5'>후기</Header>
+                        <textarea className='reviewinput' cols={50} rows={5} maxLength={200} onChange={changeReview}></textarea>
+                    </Grid.Column>
+                    <Grid.Column width={3}>
+                        <br />
+                        <Button 
+                            content='리뷰작성'
+                            color='blue'
+                            onClick={submitReview}
+                        />
+                    </Grid.Column>
+                </Grid.Row>
             </Grid>
         </>
     );
--- frontend/src/component/modal/detail.tsx
+++ frontend/src/component/modal/detail.tsx
@@ -19,12 +19,12 @@ export default function Detail({item}: any){
                     {/* <Image size='medium' src='https://next-edition.s3.amazonaws.com/theme/title_image_url/%EC%99%84%EC%A0%84%ED%95%9C%EC%82%AC%EB%9E%91(%EB%A6%AC%EB%89%B4%EC%96%BC)/theme__%E1%84%8B%E1%85%AA%E1%86%AB%E1%84%8C%E1%85%A5%E1%86%AB%E1%84%92%E1%85%A1%E1%86%AB%E1%84%89%E1%85%A1%E1%84%85%E1%85%A1%E1%86%BC-%E1%84%91%E1%85%A9%E1%84%89%E1%85%B3%E1%84%90%E1%85%A5_%EC%99%84%EC%A0%84%ED%95%9C%EC%82%AC%EB%9E%91(%EB%A6%AC%EB%89%B4%EC%96%BC).jpg' wrapped /> */}
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     <Modal.Description>
-                    <Header as='h2'>{item.name}</Header>
-                    <Header as='h3'>난이도 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <Rating icon='star' defaultRating={item.difficulty} maxRating={item.difficulty} /></Header>
-                    <Header as='h3'>장르 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {item.genre}</Header>
-                    <Header as='h3'>추천 인원 &nbsp;&nbsp;&nbsp;&nbsp; {item.reco_person}명</Header>
-                    <Header as='h3'>최대 인원 &nbsp;&nbsp;&nbsp;&nbsp; {item.max_person}명</Header>
-                    <Header as='h3'>시간 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {item.time}분</Header>
+                        <Header as='h2'>{item.name}</Header>
+                        <Header as='h3'>난이도 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <Rating icon='star' defaultRating={item.difficulty} maxRating={item.difficulty} /></Header>
+                        <Header as='h3'>장르 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {item.genre}</Header>
+                        <Header as='h3'>추천 인원 &nbsp;&nbsp;&nbsp;&nbsp; {item.reco_person}명</Header>
+                        <Header as='h3'>최대 인원 &nbsp;&nbsp;&nbsp;&nbsp; {item.max_person}명</Header>
+                        <Header as='h3'>시간 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {item.time}분</Header>
                     </Modal.Description>
                     <Modal.Description />
                     <Modal.Description>
@@ -40,11 +40,11 @@ export default function Detail({item}: any){
                         color='red'
                         onClick={() => setOpen(false)}>
                     </Button>
-                    <Button 
+                    {/* <Button 
                         content='리뷰작성'
                         color='blue'
                         onClick={() => setOpen(false)}>
-                    </Button>
+                    </Button> */}
                 </Modal.Actions>
             </Modal>
         </>
