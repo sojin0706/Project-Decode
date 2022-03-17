25a0d9f61a3111cac1f3271d32bf5b5d8b1e79eb
손민기 <smg8932@naver.com>
2022-03-17 16:35:35 +0900

    feat: 추천 페이지에 디테일 페이지 연결

--- frontend/src/component/modal/detail.tsx
+++ frontend/src/component/modal/detail.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import { useState } from 'react';
 import { Button, Header, Image, Modal, Rating } from 'semantic-ui-react'
 import Reviewboard from '../board/reviewboard';
@@ -5,7 +6,15 @@ import Reviewboard from '../board/reviewboard';
 export default function Detail({item, isImage, w, h}: any){
 
     const [open, setOpen] = useState(false)
-    console.log('111', isImage)
+
+    // var container = document.getElementById('map');
+	// 	var options = {
+	// 		center: new kakao.maps.LatLng(33.450701, 126.570667),
+	// 		level: 3
+	// 	};
+
+	// 	var map = new kakao.maps.Map(container, options);
+
     return (
         <>
             <Modal
@@ -13,12 +22,10 @@ export default function Detail({item, isImage, w, h}: any){
                 onOpen={() => setOpen(true)}
                 open={open}
                 trigger={isImage?
-                    <button><img src={item.url} alt="url" width={w} height={h} /></button>
+                    <button style={{ backgroundColor: "white", border: "white", cursor: "pointer"}}><img src={item.url} alt="url" width={w} height={h} /></button>
                 :
                     <Button>{item.name}</Button>
                 }
-                // trigger={<Button><img src={item.name} alt="url" width="150" height="150" /></Button>}
-                // trigger={<button><img src={item.url} alt="url" width="150" height="150" /></button>}
                 >
                 <Modal.Content image>
                     <img src="https://next-edition.s3.amazonaws.com/theme/title_image_url/%EC%99%84%EC%A0%84%ED%95%9C%EC%82%AC%EB%9E%91(%EB%A6%AC%EB%89%B4%EC%96%BC)/theme__%E1%84%8B%E1%85%AA%E1%86%AB%E1%84%8C%E1%85%A5%E1%86%AB%E1%84%92%E1%85%A1%E1%86%AB%E1%84%89%E1%85%A1%E1%84%85%E1%85%A1%E1%86%BC-%E1%84%91%E1%85%A9%E1%84%89%E1%85%B3%E1%84%90%E1%85%A5_%EC%99%84%EC%A0%84%ED%95%9C%EC%82%AC%EB%9E%91(%EB%A6%AC%EB%89%B4%EC%96%BC).jpg" alt="이미지가 없습니다" width="250px" height="350px"/>
@@ -26,7 +33,7 @@ export default function Detail({item, isImage, w, h}: any){
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     <Modal.Description>
                         <Header as='h2'>{item.name}</Header>
-                        <Header as='h3'>난이도 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <Rating icon='star' defaultRating={item.difficulty} maxRating={item.difficulty} /></Header>
+                        <Header as='h3'>난이도 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <Rating icon='heart' defaultRating={item.difficulty} maxRating={item.difficulty} /></Header>
                         <Header as='h3'>장르 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {item.genre}</Header>
                         <Header as='h3'>추천 인원 &nbsp;&nbsp;&nbsp;&nbsp; {item.reco_person}명</Header>
                         <Header as='h3'>최대 인원 &nbsp;&nbsp;&nbsp;&nbsp; {item.max_person}명</Header>
@@ -35,6 +42,18 @@ export default function Detail({item, isImage, w, h}: any){
                     <Modal.Description />
                     <Modal.Description>
                         <p>임시 지도 위치</p>
+                        <div id="map" style={{ width: "500", height: "400" }}></div>
+                        <h3>api{process.env.NEXT_PUBLIC_API_URL_TEST}</h3>
+                        {/* <Script type="text/javascript" src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_API_URL}`}></Script> */}
+                        {/* <Script>
+                            var container = document.getElementById('map');
+                            var options = {
+                                center: new kakao.maps.LatLng(33.450701, 126.570667),
+                                level: 3
+                            };
+
+                            var map = new kakao.maps.Map(container, options);
+                        </Script> */}
                     </Modal.Description>
                 </Modal.Content>
                 <Modal.Actions>
@@ -46,11 +65,6 @@ export default function Detail({item, isImage, w, h}: any){
                         color='red'
                         onClick={() => setOpen(false)}>
                     </Button>
-                    {/* <Button 
-                        content='리뷰작성'
-                        color='blue'
-                        onClick={() => setOpen(false)}>
-                    </Button> */}
                 </Modal.Actions>
             </Modal>
         </>
