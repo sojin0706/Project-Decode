4d5d3bb5fb4e0b321e270510c7ab5ec030f4020c
김경민 <nutmy52@naver.com>
2022-03-18 13:58:24 +0900

    fix: 테스트코드 주석

--- backend/EscapeServer/src/test/java/com/ssafy/escapesvr/EscapeServerApplicationTests.java
+++ backend/EscapeServer/src/test/java/com/ssafy/escapesvr/EscapeServerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.ssafy.escapesvr;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EscapeServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.ssafy.escapesvr;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EscapeServerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
