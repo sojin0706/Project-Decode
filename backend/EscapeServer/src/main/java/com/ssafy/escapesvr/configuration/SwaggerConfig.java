f8b2165075cbdf71ccb73247bbd25373b1035fde
김경민 <nutmy52@naver.com>
2022-03-18 14:25:45 +0900

    fix: 스웨거,서비스 오류 수정

--- backend/EscapeServer/src/main/java/com/ssafy/escapesvr/configuration/SwaggerConfig.java
+++ backend/EscapeServer/src/main/java/com/ssafy/escapesvr/configuration/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.ssafy.escapesvr.configuration;
 
-import jdk.javadoc.doclet.Doclet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
