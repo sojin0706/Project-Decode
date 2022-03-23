9f2337d9c5992be8fefaad66423d1ab87f73f154
jodie9596@gmail.com <jodie9596@gmail.com>
2022-03-23 11:16:57 +0900

    feature: additional user info gnere score base

--- frontend/pages/login/index.tsx
+++ frontend/pages/login/index.tsx
@@ -1,11 +1,20 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Form, Input, Radio, Menu, Grid, Button } from "semantic-ui-react";
+import {
+  Form,
+  Input,
+  Radio,
+  Menu,
+  Grid,
+  Button,
+  Checkbox,
+} from "semantic-ui-react";
 
 // components
 import IsLogin from "../../src/lib/customLogin";
 
 export default function Index() {
+  // 유저 정보 불러오기
   const isLogin = IsLogin;
   const [userInfo, setUserInfo]: any = useState([]);
 
@@ -30,6 +39,15 @@ export default function Index() {
     }
   }, []);
 
+  // 성별 선택
+  const [sex, setSex] = useState("M");
+
+  const handleChangeSex = (e: any) => {
+    console.log(e.target);
+    setSex(e.target.value);
+  };
+
+  // 지역 선택
   const bigPlace = [
     { key: 1, text: "서울", value: "서울" },
     { key: 2, text: "경기/인천", value: "경기/인천" },
@@ -40,28 +58,27 @@ export default function Index() {
     { key: 7, text: "제주", value: "제주" },
   ];
 
-
   const [selectedBigPlace, setSelectedBigPlace] = useState("서울");
 
   const [smallPlace, setSmallPlace] = useState([]);
-  const [selectedSmallPlace, setselectedSmallPlace] = useState('');
+  const [selectedSmallPlace, setselectedSmallPlace] = useState("");
 
-  useEffect(()=> {
+  useEffect(() => {
     axios
-    .get(`http://j6c203.p.ssafy.io:8082/information/region?largeRegion=서울`)
-    .then(({ data }) => {
-      setSmallPlace(data.smallRegions)
-      return data.smallRegions
-    })
-    .then((data)=>{
-      setselectedSmallPlace(data[0])
-    })
-  },[])
+      .get(`http://j6c203.p.ssafy.io:8082/information/region?largeRegion=서울`)
+      .then(({ data }) => {
+        setSmallPlace(data.smallRegions);
+        return data.smallRegions;
+      })
+      .then((data) => {
+        setselectedSmallPlace(data[0]);
+      });
+  }, []);
 
   const genre = [
     { key: "thrill", text: "스릴러", value: 0 },
     { key: "romance", text: "로맨스", value: 0 },
-    { key: "reasoning", text: "스릴러", value: 0 },
+    { key: "reasoning", text: "추리", value: 0 },
     { key: "sffantasy", text: "SF/판타지", value: 0 },
     { key: "adventure", text: "모험/액션", value: 0 },
     { key: "comedy", text: "코미디", value: 0 },
@@ -73,15 +90,67 @@ export default function Index() {
 
   const handleChangeBig = (e: any) => {
     axios
-      .get(`http://j6c203.p.ssafy.io:8082/information/region?largeRegion=${e.target.value}`)
+      .get(
+        `http://j6c203.p.ssafy.io:8082/information/region?largeRegion=${e.target.value}`
+      )
       .then(({ data }) => {
-        setSmallPlace(data.smallRegions)
-      })
+        setSmallPlace(data.smallRegions);
+      });
   };
 
-  const handleChangeSmall = (e:any) => {
-    setselectedSmallPlace(e.target.value)
-  }
+  const handleChangeSmall = (e: any) => {
+    setselectedSmallPlace(e.target.value);
+  };
+
+  const score = [
+    { key: 1, text: "1점", value: 1 },
+    { key: 2, text: "2점", value: 2 },
+    { key: 3, text: "3점", value: 3 },
+    { key: 4, text: "4점", value: 4 },
+    { key: 5, text: "5점", value: 5 },
+  ];
+  const [scoreThrill, setThrill] = useState(1);
+  const [scoreRomance, setRomance] = useState(1);
+  const [scoreReasoning, setReasoning] = useState(1);
+  const [scoreSffantasy, setSffantasy] = useState(1);
+  const [scoreAdventure, setAdventure] = useState(1);
+  const [scoreComedy, setComedy] = useState(1);
+  const [scoreCrime, setCrime] = useState(1);
+  const [scoreHorror, setHorror] = useState(1);
+  const [scoreAdult, setAdult] = useState(1);
+  const [scoreDrama, setDrama] = useState(1);
+
+  const handleChangeThrill = (e: any) => {
+    setThrill(e.target.value);
+  };
+  const handleChangeRomance = (e: any) => {
+    setRomance(e.target.value);
+  };
+  const handleChangeReasoning = (e: any) => {
+    setReasoning(e.target.value);
+  };
+  const handleChangeSffantasy = (e: any) => {
+    setSffantasy(e.target.value);
+  };
+  const handleChangeAdventure = (e: any) => {
+    setAdventure(e.target.value);
+  };
+  const handleChangeComedy = (e: any) => {
+    setComedy(e.target.value);
+  };
+  const handleChangeCrime = (e: any) => {
+    setCrime(e.target.value);
+  };
+  const handleChangeHorror = (e: any) => {
+    setHorror(e.target.value);
+  };
+  const handleChangeAdult = (e: any) => {
+    setAdult(e.target.value);
+  };
+  const handleChangeDrama = (e: any) => {
+    setDrama(e.target.value);
+  };
+
   return (
     <>
       <h1>{selectedBigPlace}</h1>
@@ -97,54 +166,197 @@ export default function Index() {
                   <Input placeholder={userInfo.name} />
                 </Form.Field>
               </Form.Group>
-              <Form.Group inline>
-                <Form.Field>
-                  <label>나이</label>
-                  <Input placeholder="20" />
-                </Form.Field>
-              </Form.Group>
+
               <Form.Group inline>
                 <label>성별</label>
-                <Form.Field
-                  control={Radio}
-                  label="남"
-                  value="1"
-                  // checked={value === '1'}
-                  // onChange={this.handleChange}
+                <Form.Radio
+                  label="Man"
+                  value="M"
+                  checked={sex === "M"}
+                  onChange={handleChangeSex}
                 />
-                <Form.Field
-                  control={Radio}
-                  label="여"
-                  value="2"
-                  // checked={value === '2'}
-                  // onChange={this.handleChange}
+                <Form.Radio
+                  label="Woman"
+                  value="W"
+                  checked={sex === "W"}
+                  onChange={handleChangeSex}
                 />
               </Form.Group>
-              
+
+              <div>
+                <label>성별</label>
+                <span> 남 </span>
+                <input
+                  id={"a"}
+                  value={"M"}
+                  name="platform"
+                  type="radio"
+                  checked={sex === "M"}
+                  onChange={handleChangeSex}
+                />
+                <span> 여 </span>
+                <input
+                  id={"a"}
+                  value={"W"}
+                  name="platform"
+                  type="radio"
+                  checked={sex === "W"}
+                  onChange={handleChangeSex}
+                />
+              </div>
+
+              <br></br>
+
               <div>
                 <p>대분류 지역을 선택하세요</p>
                 <select onChange={(e) => handleChangeBig(e)}>
-                  { bigPlace.map((p, i) => {
+                  {bigPlace.map((p, i) => {
                     return (
-                      <option value={p.value} key={p.key}>{p.text}</option>
-                    )
+                      <option value={p.value} key={p.key}>
+                        {p.text}
+                      </option>
+                    );
                   })}
                 </select>
               </div>
+              <br></br>
 
               <div>
                 <p>소분류 지역을 선택하세요</p>
                 <select onChange={(e) => handleChangeSmall(e)}>
-                {/* <select onChange={(e) => handleChangeBig(e)}> */}
-                  { smallPlace.map((p, i) => {
+                  {/* <select onChange={(e) => handleChangeBig(e)}> */}
+                  {smallPlace.map((p, i) => {
                     return (
-                      <option value={p} key={i}>{p}</option>
-                    )
+                      <option value={p} key={i}>
+                        {p}
+                      </option>
+                    );
                   })}
                 </select>
               </div>
-
               <br></br>
+              <div>
+                <h2>장르 선호도를 선택해주세요</h2>
+
+                <p>스릴러 {scoreThrill}</p>
+                <select onChange={(e) => handleChangeThrill(e)}>
+                  {score.map((s, i) => {
+                    return (
+                      <option value={s.value} key={i}>
+                        {s.text}
+                      </option>
+                    );
+                  })}
+                </select>
+                <br></br>
+                <br></br>
+                <p>로맨스 {scoreRomance}</p>
+                <select onChange={(e) => handleChangeRomance(e)}>
+                  {score.map((s, i) => {
+                    return (
+                      <option value={s.value} key={i}>
+                        {s.text}
+                      </option>
+                    );
+                  })}
+                </select>
+                <br></br>
+                <br></br>
+                <p>추리 {scoreReasoning}</p>
+                <select onChange={(e) => handleChangeReasoning(e)}>
+                  {score.map((s, i) => {
+                    return (
+                      <option value={s.value} key={i}>
+                        {s.text}
+                      </option>
+                    );
+                  })}
+                </select>
+                <br></br>
+                <br></br>
+                <p>SF/판타지 {scoreSffantasy}</p>
+                <select onChange={(e) => handleChangeSffantasy(e)}>
+                  {score.map((s, i) => {
+                    return (
+                      <option value={s.value} key={i}>
+                        {s.text}
+                      </option>
+                    );
+                  })}
+                </select>
+                <br></br>
+                <br></br>
+                <p>모험/액션 {scoreAdventure}</p>
+                <select onChange={(e) => handleChangeAdventure(e)}>
+                  {score.map((s, i) => {
+                    return (
+                      <option value={s.value} key={i}>
+                        {s.text}
+                      </option>
+                    );
+                  })}
+                </select>
+                <br></br>
+                <br></br>
+                <p>코미디 {scoreComedy}</p>
+                <select onChange={(e) => handleChangeComedy(e)}>
+                  {score.map((s, i) => {
+                    return (
+                      <option value={s.value} key={i}>
+                        {s.text}
+                      </option>
+                    );
+                  })}
+                </select>
+                <br></br>
+                <br></br>
+                <p>범죄 {scoreCrime}</p>
+                <select onChange={(e) => handleChangeCrime(e)}>
+                  {score.map((s, i) => {
+                    return (
+                      <option value={s.value} key={i}>
+                        {s.text}
+                      </option>
+                    );
+                  })}
+                </select>
+                <br></br>
+                <br></br>
+                <p>공포 {scoreHorror}</p>
+                <select onChange={(e) => handleChangeHorror(e)}>
+                  {score.map((s, i) => {
+                    return (
+                      <option value={s.value} key={i}>
+                        {s.text}
+                      </option>
+                    );
+                  })}
+                </select>
+                <br></br>
+                <br></br>
+                <p>19금 {scoreAdult}</p>
+                <select onChange={(e) => handleChangeAdult(e)}>
+                  {score.map((s, i) => {
+                    return (
+                      <option value={s.value} key={i}>
+                        {s.text}
+                      </option>
+                    );
+                  })}
+                </select>
+                <br></br>
+                <br></br>
+                <p>감성/드라마 {scoreDrama}</p>
+                <select onChange={(e) => handleChangeDrama(e)}>
+                  {score.map((s, i) => {
+                    return (
+                      <option value={s.value} key={i}>
+                        {s.text}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
 
               <br></br>
               <Button primary>작성</Button>
