3abe02b5ccf6c3fd42c72aac551d46a421793860
jodie9596@gmail.com <jodie9596@gmail.com>
2022-03-28 18:01:10 +0900

    feature: main data connect(themeCnt, storeCnt)

--- frontend/src/component/main/data.tsx
+++ frontend/src/component/main/data.tsx
@@ -1,22 +1,34 @@
-import { Statistic } from "semantic-ui-react"
+import { Statistic } from "semantic-ui-react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export default function data() {
+export default function Data() {
+  const [themeCnt, setThemeCnt] = useState(0)
+  const [storeCnt, setStoreCnt] = useState(0)
+
+  useEffect(() => {
+    axios.get(`http://j6c203.p.ssafy.io:8082/home`)
+    .then(({ data }) => {
+      setThemeCnt(data.count[0])
+      setStoreCnt(data.count[1])
+    });
+  }, []);
   return (
     <>
       <Statistic.Group>
-        <Statistic color='yellow'>
-          <Statistic.Value>1700</Statistic.Value>
+        <Statistic color="yellow">
+          <Statistic.Value>{themeCnt}</Statistic.Value>
           <Statistic.Label>테마 데이터</Statistic.Label>
         </Statistic>
-        <Statistic color='orange'>
-          <Statistic.Value>350</Statistic.Value>
+        <Statistic color="orange">
+          <Statistic.Value>{storeCnt}</Statistic.Value>
           <Statistic.Label>지점 데이터</Statistic.Label>
         </Statistic>
-        <Statistic color='olive'>
+        <Statistic color="olive">
           <Statistic.Value>10000</Statistic.Value>
           <Statistic.Label>유저 데이터</Statistic.Label>
         </Statistic>
       </Statistic.Group>
     </>
-  )
+  );
 }
