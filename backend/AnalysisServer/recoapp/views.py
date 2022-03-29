023f5b1df92bc160ede528463b7583eedc4c3653
손민기 <smg8932@naver.com>
2022-03-29 15:20:31 +0900

    feat: recoapp 추가

---
+++ backend/AnalysisServer/recoapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+import pymongo
+from .recommend.content import cb
+# Create your views here.
+
+
+@api_view(['GET'])
+def index(request):
+    users = pymongo.MongoClient("j6c203.p.ssafy.io", 27017).escape.users
+    theme = pymongo.MongoClient("j6c203.p.ssafy.io", 27017).escape.theme
+    review = pymongo.MongoClient("j6c203.p.ssafy.io", 27017).escape.review
+
+    result = theme.find()
+    # for r in result:
+    #     print(r)
+
+    results = cb(result)
+    context = {
+        'results': results,
+    }
+
+    return render(request, 'recoapp/index.html', context)
