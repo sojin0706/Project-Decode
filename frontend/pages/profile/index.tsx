08e0e2e0f4619de64b4d1118bfe73ebbc0d375fb
jodie9596@gmail.com <jodie9596@gmail.com>
2022-03-30 13:48:31 +0900

    feature: user Image Load

--- frontend/pages/profile/index.tsx
+++ frontend/pages/profile/index.tsx
@@ -1,12 +1,37 @@
 import { Grid, Image } from "semantic-ui-react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 // Components
+import IsLogin from "../../src/lib/customLogin";
 import Graph from "../../src/component/profile/graph"
 import RecentClear from "../../src/component/profile/recentClear";
 import ClearList from "../../src/component/profile/clearList";
 import UserInfo from "../../src/component/profile/userInfo";
 import Board from "../../src/component/profile/board";
+import UserImage from "../../src/component/profile/userImage";
 
 export default function Index() {
+  const [userInfo, setUserInfo]: any = useState([]);
+
+  useEffect(() => {
+    if (IsLogin()) {
+      var Token: any = null;
+      if (typeof window !== "undefined") Token = localStorage.getItem("token");
+
+      axios
+        .get("http://j6c203.p.ssafy.io:8081/auth/users", {
+          headers: { Authorization: `Bearer ${Token}` },
+        })
+        .then(({ data }) => {
+          setUserInfo(data.body.user)
+        })
+        .catch((e: any) => {
+          console.log("에러");
+          console.log(e);
+        });
+    }
+  }, []);
+  console.log(userInfo.image)
   return (
     <>
       <Grid stackable>
@@ -18,7 +43,7 @@ export default function Index() {
           <Grid.Column width={12}>
             <Grid centered columns={4}>
               <Grid.Column width={5}>
-                <Image src= "/images/test_chr.png" alt='test'></Image>
+                <Image src= {userInfo.image} alt='profileImage'></Image>
               </Grid.Column>
 
               <Grid.Column width={1}>
---
+++ frontend/src/component/profile/userImage.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import IsLogin from "../../lib/customLogin";
+import axios from "axios";
+import Image from "semantic-ui-react";
+
+export default function UserImage() {
+  const [userInfo, setUserInfo]: any = useState([]);
+
+  useEffect(() => {
+    if (IsLogin()) {
+      var Token: any = null;
+      if (typeof window !== "undefined") Token = localStorage.getItem("token");
+
+      axios
+        .get("http://j6c203.p.ssafy.io:8081/auth/users", {
+          headers: { Authorization: `Bearer ${Token}` },
+        })
+        .then(({ data }) => {
+          setUserInfo(data.body.user)
+        })
+        .catch((e: any) => {
+          console.log("에러");
+          console.log(e);
+        });
+    }
+  }, []);
+
+
+  return(
+    <>
+      <Image src= "/images/test_chr.png" alt='test'></Image>
+    </>
+  )
+}
