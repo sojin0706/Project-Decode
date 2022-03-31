7c1fc5f72b32fc9e9f735d82ad451797d66e7433
sojin0706 <sojin0706@naver.com>
2022-03-31 14:07:49 +0900

    feat:nonmember 테마 추가

--- backend/EscapeServer/src/main/java/com/ssafy/escapesvr/repository/ThemeRepository.java
+++ backend/EscapeServer/src/main/java/com/ssafy/escapesvr/repository/ThemeRepository.java
@@ -4,5 +4,10 @@ import com.ssafy.escapesvr.entity.Theme;
 import com.ssafy.escapesvr.repository.querydsl.InformationRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ThemeRepository extends JpaRepository<Theme,Integer>, InformationRepository {
+
+    List<Theme> findTop6ByReviewCntGreaterThanEqualOrderByScoreDesc(Integer cnt);
+
 }
--- backend/EscapeServer/src/main/java/com/ssafy/escapesvr/service/HomeServiceImpl.java
+++ backend/EscapeServer/src/main/java/com/ssafy/escapesvr/service/HomeServiceImpl.java
@@ -1,10 +1,14 @@
 package com.ssafy.escapesvr.service;
 
+import com.ssafy.escapesvr.entity.Theme;
 import com.ssafy.escapesvr.repository.StoreRepository;
 import com.ssafy.escapesvr.repository.ThemeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -28,5 +32,15 @@ public class HomeServiceImpl implements HomeService{
         count[1]=storeSize;
 
         return count;
+    }
+
+    @Override
+    public List<Integer> getNonMember() {
+        List<Theme> themes =themeRepository.findTop6ByReviewCntGreaterThanEqualOrderByScoreDesc(500);
+        List<Integer>idList=new ArrayList<>();
+        for (Theme theme : themes) {
+            idList.add(theme.getId());
+        }
+        return idList;
     }
 }
