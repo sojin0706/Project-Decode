40dd5d64eb471cf91e40d05299120310ca56eace
손민기 <smg8932@naver.com>
2022-03-31 14:32:29 +0900

    fix: settings.py

--- backend/AnalysisServer/AnalysisServer/settings.py
+++ backend/AnalysisServer/AnalysisServer/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
 
     "corsheaders",
 
-    "corsheaders.middleware.CorsMiddleware",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
