5dfa6a84ae04c5730c2f348908efb85e4ed2d856
jodie9596@gmail.com <jodie9596@gmail.com>
2022-03-31 15:11:01 +0900

    feature: profile my review

--- frontend/src/component/profile/board.tsx
+++ frontend/src/component/profile/board.tsx
@@ -1,11 +1,11 @@
-import { List, Tab } from 'semantic-ui-react'
+import { List, Tab } from "semantic-ui-react";
 import { useState, useEffect } from "react";
 import IsLogin from "../../lib/customLogin";
 import axios from "axios";
-import { useRouter } from 'next/router';
-
+import { useRouter } from "next/router";
+import Detail from "../modal/detail";
 export default function Board() {
-  const router = useRouter()
+  const router = useRouter();
   const [userInfo, setUserInfo]: any = useState(0);
 
   useEffect(() => {
@@ -26,110 +26,176 @@ export default function Board() {
         });
     }
   }, []);
-  
+
   // 리뷰 불러오기
-  const [review, setReview] = useState([])
-  const tmpReview:any = []
+  const [review, setReview]: any[] = useState([]);
+  const tmpReview: any[] = [];
   useEffect(() => {
     if (userInfo !== 0) {
-      axios.get(`http://j6c203.p.ssafy.io:8082/review/myreview/${userInfo.id}`)
-      .then(({data}) => {
-        data.review.content.map((d:any, i:number) => {
-          const tmparr:any = []
-          tmparr.push(d["createdAt"])
-          tmparr.push(d["myScore"])
-          tmparr.push(d["reviewContent"])
-          tmparr.push(d["themeId"])
-          tmparr.push(d["themeName"])
-          tmparr.push(d["themeReviewId"])
-          tmpReview.push(tmparr)
+      axios
+        .get(`http://j6c203.p.ssafy.io:8082/review/myreview/${userInfo.id}`)
+        .then(({ data }) => {
+          data.review.content.map((d: any, i: number) => {
+            console.log(d);
+            tmpReview.push(d);
+          });
         })
-        setReview(tmpReview)
-      })
+        .then(() => {
+          setReview(tmpReview);
+        });
     }
-  },[userInfo])
+  }, [userInfo]);
 
   // 유저게시판 불러오기
-  const [userBoard, setUserBoard] = useState([])
-  const tmpUserBoard:any = []
+  const [userBoard, setUserBoard] = useState([]);
+  const tmpUserBoard: any = [];
   useEffect(() => {
     if (userInfo !== 0) {
-      axios.get(`http://j6c203.p.ssafy.io:8082/article/profile/${userInfo.id}`)
-      .then(({data}) => {
-        data.myArticleList.content.map((d:any, i:number) => {
-          tmpUserBoard.push(d)
+      axios
+        .get(`http://j6c203.p.ssafy.io:8082/article/profile/${userInfo.id}`)
+        .then(({ data }) => {
+          data.myArticleList.content.map((d: any, i: number) => {
+            tmpUserBoard.push(d);
+          });
         })
-      })
-      .then(() => {
-        setUserBoard(tmpUserBoard)
-      })
+        .then(() => {
+          console.log(tmpUserBoard);
+          setUserBoard(tmpUserBoard);
+        });
     }
-  },[userInfo])
-  
+  }, [userInfo]);
+
   // 댓글 불러오기
-  const [comment, setComment] = useState([])
-  const tmpComment:any = []
+  const [comment, setComment] = useState([]);
+  const tmpComment: any = [];
   useEffect(() => {
     if (userInfo !== 0) {
-      axios.get(`http://j6c203.p.ssafy.io:8082/comment/profile/${userInfo.id}`)
-      .then(({data}) => {
-        console.log(data.myArticleCommentList.content)
-        data.myArticleCommentList.content.map((d:any, i:number) => {
-          tmpComment.push(d)
-          axios.get(`http://j6c203.p.ssafy.io:8082/article/${d.articleId}`)
-          .then(({data}) => {
-            tmpComment[i].userImage = data.article.title
-            console.log(tmpComment[i].userImage)
-          })
+      axios
+        .get(`http://j6c203.p.ssafy.io:8082/comment/profile/${userInfo.id}`)
+        .then(({ data }) => {
+          data.myArticleCommentList.content.map((d: any, i: number) => {
+            tmpComment.push(d);
+            axios
+              .get(`http://j6c203.p.ssafy.io:8082/article/${d.articleId}`)
+              .then(({ data }) => {
+                tmpComment[i].userImage = data.article.title;
+              });
+          });
         })
-      })
-      .then(() => {
-        setComment(tmpComment)
-      })
+        .then(() => {
+          setComment(tmpComment);
+        });
     }
-  },[userInfo])
+  }, [userInfo]);
 
-const panes = [
-  {
-    menuItem: '리뷰',
-    render: () => <Tab.Pane attached={false}>
-      <ul>
-        <p>{review}</p>
-      </ul>
-    </Tab.Pane>
-  },
-  {
-    menuItem: '유저 게시판',
-    render: () => <Tab.Pane attached={false}>내가 작성한 유저 게시판 글
-    <ul>
-      {userBoard.map((d:any, i: number) => {
-        return (
-          <li key={i} onClick = {() => {router.push(`/userboard/${d.id}`)}} style = {{cursor: 'pointer'}}>번호: {d.id} 제목: {d.title} 내용: {d.content}</li>
-        )
-      })}
-    </ul>
-    </Tab.Pane>,
-  },
-  {
-    menuItem: '댓글',
-    render: () => <Tab.Pane attached={false}>내가 작성한 댓글
-        <ul>
-      {comment.map((d:any, i: number) => {
-        return (
-          <li key={i} onClick = {() => {router.push(`/userboard/${d.articleId}`)}} style = {{cursor: 'pointer'}}> 글 제목: {d.userImage} 댓글 내용: {d.content}</li>
-        )
-      })}
-    </ul>
-    </Tab.Pane>,
-  },
-  {
-    menuItem: 'Q&A',
-    render: () => <Tab.Pane attached={false}>내가 작성한 Q&A 글</Tab.Pane>,
-  },
-]
+  // Q&A 불러오기
+  const [qa, setQA] = useState([]);
+  const tmpQA: any = [];
+  useEffect(() => {
+    if (userInfo !== 0) {
+      axios
+        .get(`http://j6c203.p.ssafy.io:8082/qna/profile/${userInfo.id}`)
+        .then(({ data }) => {
+          data.myQnaList.content.map((d: any, i: number) => {
+            tmpQA.push(d);
+          });
+        })
+        .then(() => {
+          setQA(tmpQA);
+        });
+    }
+  }, [userInfo]);
 
+  const panes = [
+    {
+      menuItem: "리뷰",
+      render: () => (
+        <Tab.Pane attached={false}>
+          <ul>
+            {review.map((d: any, i: number) => {
+              return (
+                <li key={i} style={{ cursor: "pointer" }}>
+                  번호: {d.themeReviewId} 테마:{" "}
+                  <Detail themeId={d.themeId} isImage={false} w={150} h={200} />{" "}
+                  내용: {d.reviewContent} 내가 준 평점: {d.myScore}
+                </li>
+              );
+            })}
+          </ul>
+        </Tab.Pane>
+      ),
+    },
+    {
+      menuItem: "유저 게시판",
+      render: () => (
+        <Tab.Pane attached={false}>
+          내가 작성한 유저 게시판 글
+          <ul>
+            {userBoard.map((d: any, i: number) => {
+              return (
+                <li
+                  key={i}
+                  onClick={() => {
+                    router.push(`/userboard/${d.id}`);
+                  }}
+                  style={{ cursor: "pointer" }}
+                >
+                  번호: {d.id} 제목: {d.title} 내용: {d.content}
+                </li>
+              );
+            })}
+          </ul>
+        </Tab.Pane>
+      ),
+    },
+    {
+      menuItem: "댓글",
+      render: () => (
+        <Tab.Pane attached={false}>
+          내가 작성한 댓글
+          <ul>
+            {comment.map((d: any, i: number) => {
+              return (
+                <li
+                  key={i}
+                  onClick={() => {
+                    router.push(`/userboard/${d.articleId}`);
+                  }}
+                  style={{ cursor: "pointer" }}
+                >
+                  {" "}
+                  글 제목: {d.userImage} 댓글 내용: {d.content}
+                </li>
+              );
+            })}
+          </ul>
+        </Tab.Pane>
+      ),
+    },
+    {
+      menuItem: "Q&A",
+      render: () => (
+        <Tab.Pane attached={false}>
+          내가 작성한 Q&A 글
+          <ul>
+            {qa.map((d: any, i: number) => {
+              return (
+                <li
+                  key={i}
+                  onClick={() => {
+                    router.push(`/notice/qna/${d.id}`);
+                  }}
+                  style={{ cursor: "pointer" }}
+                >
+                  번호: {d.id} 제목: {d.title} 내용: {d.content}
+                </li>
+              );
+            })}
+          </ul>
+        </Tab.Pane>
+      ),
+    },
+  ];
 
-return (
-  <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
-)
+  return <Tab menu={{ secondary: true, pointing: true }} panes={panes} />;
 }
