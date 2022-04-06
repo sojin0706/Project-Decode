22d62a2a469c993ac48e787e323d9c1a5009b622
김경민 <nutmy52@naver.com>
2022-04-07 01:56:09 +0900

    build: prod 환경 setting

--- backend/GateWayServer/src/main/java/com/ssafy/gatewaysvr/filter/LoggingFilter.java
+++ backend/GateWayServer/src/main/java/com/ssafy/gatewaysvr/filter/LoggingFilter.java
@@ -11,7 +11,6 @@ import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
-// 공통 필터 - 모든 경우에 이 필터를 거친다.
 @Component
 @Slf4j
 public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
@@ -21,27 +20,6 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
 
     @Override
     public GatewayFilter apply(Config config) {
-
-//        return (exchange, chain) -> {
-//            // exchange 객체로 request, response를 받는다.
-//            ServerHttpRequest request = exchange.getRequest();
-//            ServerHttpResponse response = exchange.getResponse();
-//
-//            log.info("Global filter: request id -> {}", config.getBaseMessage());
-//
-//            if (config.isPreLogger()){
-//                log.info("Global start filter: request id -> {}", request.getId());
-//            }
-//
-//            // Custom post filter
-//            // chain에다가 postfilter: exchange 추가
-//            // 비동기방식으로 단일값 추가히기 위해서 Mono: 웹 플럭스 타입으로 추가
-//            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-//                if (config.isPostLogger()){
-//                    log.info("Global End filter: response code -> {}", response.getStatusCode());
-//                }
-//            }));
-//        };
         GatewayFilter filter = new OrderedGatewayFilter((exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
@@ -52,7 +30,6 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
                 log.info("Logging PRE filter: request id -> {}", request.getId());
             }
 
-            // Custom post filter
             // chain에다가 postfilter: exchange 추가
             // 비동기방식으로 단일값 추가히기 위해서 Mono: 웹 플럭스 타입으로 추가
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
@@ -65,7 +42,6 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
         return filter;
     }
 
-    // inner class로 설정
     @Data
     public static class Config {
         private String baseMessage;
