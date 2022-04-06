2b497a0a0d54d9100402ed1708410f5b35630adf
김경민 <nutmy52@naver.com>
2022-04-06 09:18:34 +0900

    build: analysis-server-remove

--- frontend/src/lib/recoAxios.tsx
+++ frontend/src/lib/recoAxios.tsx
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const LOCAL_API_URL = "http://localhost:8080";
-const SERVER_API_URL = "http://j6c203.p.ssafy.io:8083/recoapp";
+const SERVER_API_URL = "https://j6c203.p.ssafy.io/api/analysis-server/recoapp";
 
 const recoAxios = Axios.create({
   baseURL: `${SERVER_API_URL}`,
